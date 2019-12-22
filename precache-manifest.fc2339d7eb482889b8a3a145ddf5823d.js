52a5a32352746b567d5050f7c527813897a74f24
tainguyen197 <tainguyen197.ntt@gmail.com>
2019-12-22 14:01:36 +0700

    Updates

---
+++ precache-manifest.fc2339d7eb482889b8a3a145ddf5823d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5372494a4294e1ec7e4b799b17dcbd72",
+    "url": "/cameraShop/index.html"
+  },
+  {
+    "revision": "5f6fc9d3dbce05396507",
+    "url": "/cameraShop/static/css/2.dcfc59fc.chunk.css"
+  },
+  {
+    "revision": "579bd5872f560b4755ff",
+    "url": "/cameraShop/static/css/main.1fcec0fc.chunk.css"
+  },
+  {
+    "revision": "5f6fc9d3dbce05396507",
+    "url": "/cameraShop/static/js/2.7bf846dc.chunk.js"
+  },
+  {
+    "revision": "f8325d336fa956780f66870e00d58912",
+    "url": "/cameraShop/static/js/2.7bf846dc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "579bd5872f560b4755ff",
+    "url": "/cameraShop/static/js/main.641589bb.chunk.js"
+  },
+  {
+    "revision": "80574aae11688f89ae20",
+    "url": "/cameraShop/static/js/runtime-main.3b2dc8df.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cameraShop/static/media/logo.5d5d9eef.svg"
+  }
+]);
