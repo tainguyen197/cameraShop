fb9b445a108a475f785a954d3c396121f5c1904e
tainguyen197 <tainguyen197.ntt@gmail.com>
2019-12-29 11:28:23 +0700

    custoom brands section

--- src/container/SectionBrands/index.jsx
+++ src/container/SectionBrands/index.jsx
@@ -1,6 +1,6 @@
 import React, { Fragment, useMemo, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-
+import './styles.css'
 import * as brandAction from "../../action/brand";
 
 const SectionBrand = props => {
@@ -14,8 +14,16 @@ const SectionBrand = props => {
     dispatch(brandAction.loadBrand());
   }, []);
 
-  const data = brandStore.data;
+  const data = (Array.isArray(brandStore.data)?brandStore.data:[]);
+console.log(data);
 
+  const listBrand = data.map(item=>{
+    return(
+      <div className="brand-box"><img src={item.src[0]}></img></div>
+    )
+  })
+
+  
   return (
     <Fragment>
       <section className="banner bgwhite p-t-40 p-b-40">
@@ -23,109 +31,8 @@ const SectionBrand = props => {
           <div className="sec-title p-b-60">
             <h3 className="m-text5 t-center">Các thương hiệu</h3>
           </div>
-          <div className="row">
-            <div className="col-sm-10 col-md-8 col-lg-4 m-l-r-auto">
-              {/* block1 */}
-              <div className="block1 hov-img-zoom pos-relative m-b-30">
-                <img
-                  src={data ? data[0].src[0] : "images/banner-05.jpg"}
-                  alt="IMG-BENNER"
-                />
-                <div className="block1-wrapbtn w-size2">
-                  {/* Button */}
-                  <a
-                    href="#"
-                    className="flex-c-m size2 m-text2 bg3 hov1 trans-0-4"
-                  >
-                    {data ? data[0].name : "unName"}
-                  </a>
-                </div>
-              </div>
-              {/* block1 */}
-              <div className="block1 hov-img-zoom pos-relative m-b-30">
-                <img
-                  src={data ? data[1].src[0] : "images/banner-05.jpg"}
-                  alt="IMG-BENNER"
-                />
-                <div className="block1-wrapbtn w-size2">
-                  {/* Button */}
-                  <a
-                    href="#"
-                    className="flex-c-m size2 m-text2 bg3 hov1 trans-0-4"
-                  >
-                    {data ? data[1].name : "unName"}
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="col-sm-10 col-md-8 col-lg-4 m-l-r-auto">
-              {/* block1 */}
-              <div className="block1 hov-img-zoom pos-relative m-b-30">
-                <img
-                  src={data ? data[2].src[0] : "images/banner-05.jpg"}
-                  alt="IMG-BENNER"
-                />
-                <div className="block1-wrapbtn w-size2">
-                  {/* Button */}
-                  <a
-                    href="#"
-                    className="flex-c-m size2 m-text2 bg3 hov1 trans-0-4"
-                  >
-                    {data ? data[2].name : "unName"}
-                  </a>
-                </div>
-              </div>
-              {/* block1 */}
-              <div className="block1 hov-img-zoom pos-relative m-b-30">
-                <img
-                  src={data ? data[3].src[0] : "images/banner-05.jpg"}
-                  alt="IMG-BENNER"
-                />
-                <div className="block1-wrapbtn w-size2">
-                  {/* Button */}
-                  <a
-                    href="#"
-                    className="flex-c-m size2 m-text2 bg3 hov1 trans-0-4"
-                  >
-                    {data ? data[3].name : "unName"}
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="col-sm-10 col-md-8 col-lg-4 m-l-r-auto">
-              {/* block1 */}
-              <div className="block1 hov-img-zoom pos-relative m-b-30">
-                <img
-                  src={data ? data[3].src[0] : "images/banner-05.jpg"}
-                  alt="IMG-BENNER"
-                />
-                <div className="block1-wrapbtn w-size2">
-                  {/* Button */}
-                  <a
-                    href="#"
-                    className="flex-c-m size2 m-text2 bg3 hov1 trans-0-4"
-                  >
-                    {data ? data[4].name : "unName"}
-                  </a>
-                </div>
-              </div>
-              {/* block2 */}
-              <div className="block1 hov-img-zoom pos-relative m-b-30">
-                <img
-                  src={data ? data[3].src[0] : "images/banner-05.jpg"}
-                  alt="IMG-BENNER"
-                />
-                <div className="block1-wrapbtn w-size2">
-                  {/* Button */}
-                  <a
-                    href="#"
-                    className="flex-c-m size2 m-text2 bg3 hov1 trans-0-4"
-                  >
-                    {data ? data[4].name : "unName"}
-                  </a>
-                </div>
-              </div>
-              </div>
+          <div className="row section-brand-box">
+            {listBrand}
           </div>
         </div>
       </section>
