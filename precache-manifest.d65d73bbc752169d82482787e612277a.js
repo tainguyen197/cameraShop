25bc469705d97c91ff0be14f7a50373a0af7a718
tainguyen197 <tainguyen197.ntt@gmail.com>
2019-12-30 20:06:17 +0700

    Updates

---
+++ precache-manifest.d65d73bbc752169d82482787e612277a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a0575ef0d8b9316ad9c2206104fc273",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54c129cd81614eaa44e7",
+    "url": "/static/css/2.de4bead4.chunk.css"
+  },
+  {
+    "revision": "ed4b42c42de36275209e",
+    "url": "/static/css/main.dba6a278.chunk.css"
+  },
+  {
+    "revision": "54c129cd81614eaa44e7",
+    "url": "/static/js/2.8d036081.chunk.js"
+  },
+  {
+    "revision": "164526e261ad99095f4c85ffd3662517",
+    "url": "/static/js/2.8d036081.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ed4b42c42de36275209e",
+    "url": "/static/js/main.c5e6e4ba.chunk.js"
+  },
+  {
+    "revision": "f0f5d47ef4b606c6d27c",
+    "url": "/static/js/runtime-main.87f1a152.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cameraShop/precache-manifest.5f1c5a84acfc53b18ab9084fe14622c8.js"
+  "/precache-manifest.d65d73bbc752169d82482787e612277a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cameraShop/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
