b42f6f7f7fbaf375645f852744066022a9bfa3fc
tainguyen197 <tainguyen197.ntt@gmail.com>
2019-12-31 21:10:57 +0700

    clear heart icon from homepage

--- src/component/ProductCard/index.jsx
+++ src/component/ProductCard/index.jsx
@@ -22,10 +22,6 @@ const ProductCard = props => {
           </div>
         </div>
         <div class="block2-overlay trans-0-4">
-          <a href="#" class="block2-btn-addwishlist hov-pointer trans-0-4">
-            <i class="icon-wishlist icon_heart_alt" aria-hidden="true"></i>
-            <i class="icon-wishlist icon_heart dis-none" aria-hidden="true"></i>
-          </a>
 
           <div class="block2-btn-addcart w-size1 trans-0-4">
             <button class="flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4">
