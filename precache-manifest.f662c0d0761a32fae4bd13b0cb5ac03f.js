d2844048557467f040671c17029065ddfffaa16b
tainguyen197 <tainguyen197.ntt@gmail.com>
2020-01-01 21:30:37 +0700

    Updates

---
+++ precache-manifest.f662c0d0761a32fae4bd13b0cb5ac03f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e2cccac111727315f09be9159527dc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39f7b3495946604d5e50",
+    "url": "/static/css/2.de4bead4.chunk.css"
+  },
+  {
+    "revision": "cefbcc68e0e909828ee5",
+    "url": "/static/css/main.2cdabe77.chunk.css"
+  },
+  {
+    "revision": "39f7b3495946604d5e50",
+    "url": "/static/js/2.62a50a1a.chunk.js"
+  },
+  {
+    "revision": "164526e261ad99095f4c85ffd3662517",
+    "url": "/static/js/2.62a50a1a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cefbcc68e0e909828ee5",
+    "url": "/static/js/main.01484986.chunk.js"
+  },
+  {
+    "revision": "f0f5d47ef4b606c6d27c",
+    "url": "/static/js/runtime-main.87f1a152.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2242ead420728182f418e0c596b4ab3d.js"
+  "/precache-manifest.f662c0d0761a32fae4bd13b0cb5ac03f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
