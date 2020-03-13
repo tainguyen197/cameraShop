f5e5bdd1d611fa99dca9ffe85a443de28b5df91f
ivy-multime86 <ivy.multime86@gmail.com>
2020-03-14 00:01:47 +0700

    Updates

--- precache-manifest.16e636cb8d2a92ee65f25071d4b9dc0d.js
+++ precache-manifest.16e636cb8d2a92ee65f25071d4b9dc0d.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "23df6f2354a2885704b784367f428439",
+    "revision": "e03d0d345e156f0f7d618f66b07b493d",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.8f0a820b.chunk.css"
   },
   {
-    "revision": "4f7125bcde51102deb95",
+    "revision": "4483116d597767c4b87f",
     "url": "/static/css/main.af25ee20.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.e22a8e4d.chunk.js.LICENSE"
   },
   {
-    "revision": "4f7125bcde51102deb95",
-    "url": "/static/js/main.425bfb8b.chunk.js"
+    "revision": "4483116d597767c4b87f",
+    "url": "/static/js/main.b9fe7c37.chunk.js"
   },
   {
     "revision": "f0f5d47ef4b606c6d27c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.91497edb97c9bd9b17805bf085a056c9.js"
+  "/precache-manifest.16e636cb8d2a92ee65f25071d4b9dc0d.js"
 );
 
 self.addEventListener('message', (event) => {
